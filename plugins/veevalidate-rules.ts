21b4b17b909a7487d3704363a8774abcde166776
Tinh Nguyen <tinh@mgisolutions.com>
2022-09-20 08:37:37 +0700

    remove plugin

--- plugins/veevalidate-rules.ts
+++
@@ -1,9 +0,0 @@
-import {defineRule} from "vee-validate";
-import * as rules from "@vee-validate/rules";
-
-export default defineNuxtPlugin(() => {
-    Object.keys(rules).filter((k) => k !== 'default')
-    .forEach(rule => {
-        defineRule(rule, rules[rule]);
-    })
-})
